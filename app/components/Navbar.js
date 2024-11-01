e6c387ebfbd5c827f98159fa33f7aefcc3f4647b
Jaydeep Baldaniya <jaydeepbaldaniya08@gmail.com>
2024-11-01 20:17:19 +0530

    temp

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -70,7 +70,7 @@ const Navbar = () => {
                     href={`/${session.user.name}`}
                     className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                   >
-                    Your Page
+                    Your Pages
                   </Link>
                 </li>
 
